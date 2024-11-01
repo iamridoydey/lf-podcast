840deb0eb76e4988bc44fbea37b6486a08ab3f6e
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-11-01 18:51:26 +0600

    added: Add Ashley Wolf, GitHub, Episode to Project Listings #36
    Signed-off-by: Ridoy Chandra Dey <iamridoydey@gmail.com>

--- website/src/components/podcasts.js
+++ website/src/components/podcasts.js
@@ -1,121 +1,146 @@
-import React from 'react';
+import React from "react";
 
-export const podcasts = { 
-    "gab" : {
-        title : "Is the finance industry using open source? Yes. Yes it is!, with Gabriele Columbro",
-        src : "https://open.spotify.com/embed/episode/5dN7v4YkpNJbXjVlOh2VNh?utm_source=generator",
-        type : "spotify",
-        description : "With major software vulnerabilities popping up on what seems like a weekly basis and government regulation imminent when it comes to providing a software bill of materials for any application sold to the United States government, collaboration on open source security is no longer optional.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "ana" : {
-        title : "Become a Hybrid in the Open Source Community, with Ana Jiménez",
-        src : "https://open.spotify.com/embed/episode/74DIeDdIDwgn6HicgyTlMy?utm_source=generator",
-        type : "spotify",
-        description : "I usually say that I'm a hybrid, Ana Jiménez says. In this context what does that even mean, what is a hybrid? According to the Oxford Language Dictionary, a hybrid is 'a word formed from elements taken from different languages, for example television ( tele- from Greek, vision from Latin).' If we use that as our definition, Ana Jiménez Santamaria has a good reason to call herself a hybrid; she can speak the language of the business world as well as that of the developer domain.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "allan" : {
-        title : "Waiting for the SBOM to Drop, with Allan Friedman",
-        src : "https://open.spotify.com/embed/episode/02PpSGGsuD9kTbVQStquM9?utm_source=generator",
-        type : "spotify",
-        description : "Allan Friedman was one of the first, if not THE first person to talk with me about the need for a mandatory software bill of materials to be attached to all software back in 2017 when he was Director of Cybersecurity Initiatives for the US Department of National Telecommunications and Information Administration (NTIA).",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "arpit" : {
-        title : "LFNetworking and Edge Computing, with Arpit Joshipura",
-        src : "https://open.spotify.com/embed/episode/7ws4g9oV0Z6cksVC4OICvT?utm_source=generator",
-        type : "spotify",
-        description : "Ahmedabad is the largest city in the state of Gujarat (goo jer raht) in western India. It has a population of over eight million people. This is where Arpit Joshipura, GM of LFNetworking at the Linux Foundation, was born and raised.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "daniel" : {
-        title : "The Call for Code Project, with Daniel Krook",
-        src : "https://open.spotify.com/embed/episode/05TyLnJ2OXYdq6hmk5vPbW?utm_source=generator",
-        type : "spotify",
-        description : "Technology influences every aspect of our life. It's hard to remember a time when analog was separate from the digital. How do we balance the pace of innovation with its social impact when everything is changing so quickly?",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "royal" : {
-        title : "Games and Digital Media in Open Source, with Royal O'Brien",
-        src : "https://open.spotify.com/embed/episode/2q8dXeMQLeW8sTXHGn8upP?utm_source=generator",
-        type : "spotify",
-        description : "The Unreal gaming engine launched in 1998. It was a fun time. It was like, “Oh my God, we can build our own games and gaming maps!” But those earlier in the gaming cycle thought there was a better alternative already on the market.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "nithya" : {
-        title : "Building Your Open Source Strategy, with Nithya Ruff",
-        src : "https://open.spotify.com/embed/episode/47Ea6XOrwyFJ1dYZnzgR7G?utm_source=generator",
-        type : "spotify",
-        description : "A lot of the companies I've worked for were trying to transition from proprietary or very siloed products, very black box products to more standards based products. And that seems to be a common thread that I've gone through even in proprietary companies.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "sara" : {
-        title : "Management and Diversity in the Open Source Community with Sara Chipps",
-        src : "https://open.spotify.com/embed/episode/5Z5FT3qzBrbTvAyHuhNq9G?utm_source=generator",
-        type : "spotify",
-        description : "As Sara Chipps delved deeper into the world of open source and moved up into senior engineering and management roles, there was a specific skill she learned about how to work with and manage engineers.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "clyde" : {
-        title : "A New Model for Technical Training, with Clyde Seepersad",
-        src : "https://open.spotify.com/embed/episode/2HCRrlO0zW2xEcfnZ5i0fG?utm_source=generator",
-        type : "spotify",
-        description : "Clyde Seepersad is the Senior Vice President & General Manager, of the Training & Certification Project at The Linux Foundation. He carries the idea with him that failure is temporary. Knowing that can help you get through some pretty intense situations.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "patrick" : {
-        title : "The Business Side of Open Source, with Patrick Debois",
-        src : "https://open.spotify.com/embed/episode/3MKsXkw9Et5B9bGLWKJNpc?utm_source=generator",
-        type : "spotify",
-        description : "The first time Patrick Debois came into contact with Open Source was in the early stages of development of the Linux kernel, compiling it on floppies on his 486 machine. To tell you how long ago that was, the Intel 486 was introduced in 1989, It was the first chip in the line to include a built-in math coprocessor.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "brian" : {
-        title : "A Life in Open Source, with Brian Behlendorf",
-        src : "https://open.spotify.com/embed/episode/0P0cjBDn5nSYPe1i0FrFXr?utm_source=generator",
-        type : "spotify",
-        description : "Brian Behlendorf came from a science and technology background. In fact, his parents met at IBM where his father was a Cobol programmer. During the 1980s, Brian was comfortable in front of a TRS 80 and a PC junior doing basic programming and term reports.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "priyanka" : {
-        title : "Balancing Priorities at the CNCF, with Priyanka Sharma",
-        src : "https://open.spotify.com/embed/episode/5KgqNXHHV0y03yjgg7kg8E?utm_source=generator",
-        type : "spotify",
-        description : "Priyanka Sharma has had a long career in tech. After graduating from Stanford in 2009, she worked at Google in the Online Partnerships Group, was a technical consultant where she onboarded new DoubleClick clients, and acted as interim Product Manager for internal insights tools.",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    },
-    "intro" : {
-        title : "The Untold Stories of Open Source - An Introduction to the new Podcast from the Linux Foundation",
-        src : "https://open.spotify.com/embed/episode/62DAkdeQTSAPeLbxF2sTlX?utm_source=generator",
-        type : "spotify",
-        description : "Open Source is embedded in every software application you touch today. It's impossible to build a large scale application without it. The real question is, what's the story behind that component, application, or framework you just downloaded? Not the specs. Not the functionality. The real story: 'Who wrote the code? What is their backstory? What led them to the Open Source community?'",
-        podcast : "The Untold Stories of Open Source",
-        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
-        featured : true
-    }  
+export const podcasts = {
+  gab: {
+    title:
+      "Is the finance industry using open source? Yes. Yes it is!, with Gabriele Columbro",
+    src: "https://open.spotify.com/embed/episode/5dN7v4YkpNJbXjVlOh2VNh?utm_source=generator",
+    type: "spotify",
+    description:
+      "With major software vulnerabilities popping up on what seems like a weekly basis and government regulation imminent when it comes to providing a software bill of materials for any application sold to the United States government, collaboration on open source security is no longer optional.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  ana: {
+    title: "Become a Hybrid in the Open Source Community, with Ana Jiménez",
+    src: "https://open.spotify.com/embed/episode/74DIeDdIDwgn6HicgyTlMy?utm_source=generator",
+    type: "spotify",
+    description:
+      "I usually say that I'm a hybrid, Ana Jiménez says. In this context what does that even mean, what is a hybrid? According to the Oxford Language Dictionary, a hybrid is 'a word formed from elements taken from different languages, for example television ( tele- from Greek, vision from Latin).' If we use that as our definition, Ana Jiménez Santamaria has a good reason to call herself a hybrid; she can speak the language of the business world as well as that of the developer domain.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  allan: {
+    title: "Waiting for the SBOM to Drop, with Allan Friedman",
+    src: "https://open.spotify.com/embed/episode/02PpSGGsuD9kTbVQStquM9?utm_source=generator",
+    type: "spotify",
+    description:
+      "Allan Friedman was one of the first, if not THE first person to talk with me about the need for a mandatory software bill of materials to be attached to all software back in 2017 when he was Director of Cybersecurity Initiatives for the US Department of National Telecommunications and Information Administration (NTIA).",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  arpit: {
+    title: "LFNetworking and Edge Computing, with Arpit Joshipura",
+    src: "https://open.spotify.com/embed/episode/7ws4g9oV0Z6cksVC4OICvT?utm_source=generator",
+    type: "spotify",
+    description:
+      "Ahmedabad is the largest city in the state of Gujarat (goo jer raht) in western India. It has a population of over eight million people. This is where Arpit Joshipura, GM of LFNetworking at the Linux Foundation, was born and raised.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  daniel: {
+    title: "The Call for Code Project, with Daniel Krook",
+    src: "https://open.spotify.com/embed/episode/05TyLnJ2OXYdq6hmk5vPbW?utm_source=generator",
+    type: "spotify",
+    description:
+      "Technology influences every aspect of our life. It's hard to remember a time when analog was separate from the digital. How do we balance the pace of innovation with its social impact when everything is changing so quickly?",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  royal: {
+    title: "Games and Digital Media in Open Source, with Royal O'Brien",
+    src: "https://open.spotify.com/embed/episode/2q8dXeMQLeW8sTXHGn8upP?utm_source=generator",
+    type: "spotify",
+    description:
+      "The Unreal gaming engine launched in 1998. It was a fun time. It was like, “Oh my God, we can build our own games and gaming maps!” But those earlier in the gaming cycle thought there was a better alternative already on the market.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  nithya: {
+    title: "Building Your Open Source Strategy, with Nithya Ruff",
+    src: "https://open.spotify.com/embed/episode/47Ea6XOrwyFJ1dYZnzgR7G?utm_source=generator",
+    type: "spotify",
+    description:
+      "A lot of the companies I've worked for were trying to transition from proprietary or very siloed products, very black box products to more standards based products. And that seems to be a common thread that I've gone through even in proprietary companies.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  sara: {
+    title:
+      "Management and Diversity in the Open Source Community with Sara Chipps",
+    src: "https://open.spotify.com/embed/episode/5Z5FT3qzBrbTvAyHuhNq9G?utm_source=generator",
+    type: "spotify",
+    description:
+      "As Sara Chipps delved deeper into the world of open source and moved up into senior engineering and management roles, there was a specific skill she learned about how to work with and manage engineers.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  clyde: {
+    title: "A New Model for Technical Training, with Clyde Seepersad",
+    src: "https://open.spotify.com/embed/episode/2HCRrlO0zW2xEcfnZ5i0fG?utm_source=generator",
+    type: "spotify",
+    description:
+      "Clyde Seepersad is the Senior Vice President & General Manager, of the Training & Certification Project at The Linux Foundation. He carries the idea with him that failure is temporary. Knowing that can help you get through some pretty intense situations.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  patrick: {
+    title: "The Business Side of Open Source, with Patrick Debois",
+    src: "https://open.spotify.com/embed/episode/3MKsXkw9Et5B9bGLWKJNpc?utm_source=generator",
+    type: "spotify",
+    description:
+      "The first time Patrick Debois came into contact with Open Source was in the early stages of development of the Linux kernel, compiling it on floppies on his 486 machine. To tell you how long ago that was, the Intel 486 was introduced in 1989, It was the first chip in the line to include a built-in math coprocessor.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  brian: {
+    title: "A Life in Open Source, with Brian Behlendorf",
+    src: "https://open.spotify.com/embed/episode/0P0cjBDn5nSYPe1i0FrFXr?utm_source=generator",
+    type: "spotify",
+    description:
+      "Brian Behlendorf came from a science and technology background. In fact, his parents met at IBM where his father was a Cobol programmer. During the 1980s, Brian was comfortable in front of a TRS 80 and a PC junior doing basic programming and term reports.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  priyanka: {
+    title: "Balancing Priorities at the CNCF, with Priyanka Sharma",
+    src: "https://open.spotify.com/embed/episode/5KgqNXHHV0y03yjgg7kg8E?utm_source=generator",
+    type: "spotify",
+    description:
+      "Priyanka Sharma has had a long career in tech. After graduating from Stanford in 2009, she worked at Google in the Online Partnerships Group, was a technical consultant where she onboarded new DoubleClick clients, and acted as interim Product Manager for internal insights tools.",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  intro: {
+    title:
+      "The Untold Stories of Open Source - An Introduction to the new Podcast from the Linux Foundation",
+    src: "https://open.spotify.com/embed/episode/62DAkdeQTSAPeLbxF2sTlX?utm_source=generator",
+    type: "spotify",
+    description:
+      "Open Source is embedded in every software application you touch today. It's impossible to build a large scale application without it. The real question is, what's the story behind that component, application, or framework you just downloaded? Not the specs. Not the functionality. The real story: 'Who wrote the code? What is their backstory? What led them to the Open Source community?'",
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
+  ashley: {
+    title: "Managing an Open Source Program Office, with Ashley Wolf",
+    src: "https://open.spotify.com/embed/episode/12rMujLaSN70blkoZOpy2J?utm_source=generator",
+    type: "spotify",
+    description: `It’s a consistent pattern at most companies: High-value data and corporate memory are stored in isolated channels on disparate systems. Old processes are protected by those who have been there the longest. The problem is, the DNA of the company becomes lost as long-time employees depart, making it difficult for new hires to find what is available, why decisions were made, and who they can look to for answers.`,
+    podcast: "The Untold Stories of Open Source",
+    podcastUrl: "https://untold-stories-of-open-source.captivate.fm/spotify",
+    featured: true,
+  },
 };
